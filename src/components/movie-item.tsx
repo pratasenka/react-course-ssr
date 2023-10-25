aad172210becf9a26e7ec361a7f8541b297c335a
Anton Pratasenka <anton_pratasenka@epam.com>
2023-10-25 10:41:34 +0300

    fixed after review

--- src/components/movie-item.tsx
+++ src/components/movie-item.tsx
@@ -1,33 +1,13 @@
 'use client'
 
-// import { useRouter } from 'next/navigation';
 import styles from '../styles/movie-item.module.css';
 
 export default function MovieItem(props: any) {
-    // const router = useRouter();
-
     return <>
         <div
             className={styles["movie-item"]}
-        // onClick={() => router.push(`/${props.movie.id}`)}
         >
             <div className={styles["movie-item-image-container"]}>
-                {/* <div className={`${styles["dropdown"]} ${styles["dropdown-position"]}`}>
-                        <button className={styles["dropbtn"]}>x</button>
-                        <div className={styles["dropdown-content"]}>
-                            <a onClick={(e) => {
-                            e.stopPropagation();
-                            props.edit(props.movie.id);
-                        }}
-                        >Edit</a>
-
-                        <a onClick={(e) => {
-                            e.stopPropagation()
-                            props.delete(props.movie.id)
-                        }}
-                        >Delete</a>
-                        </div>
-                    </div> */}
                 <img
                     className={styles["movie-item-image"]}
                     src={props.movie.imageUrl}
@@ -45,7 +25,6 @@ export default function MovieItem(props: any) {
                 <div className={styles["movie-item-detail-row"]}>
                     <span className={styles["movie-item-genre"]}>
                         {props.movie.relevantGenres.join(', ')}
-                        {/* {props.movie.relevantGenres.map((genre: string) => capitalize(genre)).join(', ')} */}
                     </span>
                 </div>
             </div>
